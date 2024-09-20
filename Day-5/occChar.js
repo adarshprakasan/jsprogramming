0a605b3114d78132a51d0d0f992f390a65b0019f
Adarsh <53871621+adarshprakasan@users.noreply.github.com>
2024-09-20 22:49:08 +0530

    string occurance for multiple number

--- Day-5/occChar.js
+++ Day-5/occChar.js
@@ -1,15 +1,51 @@
 //? Print character according to the number in a given string
+//! consider there is only one number after each char
 
-function occChar(str) {
-  let res = "";
-  for (let i = 0; i < str.length; i += 2) {
-    let count = Number(str[i + 1]);
-    let temp = "";
-    for (let j = 0; j < count; j++) {
-      temp += str[i];
+// function occChar(str) {
+//   let res = "";
+//   for (let i = 0; i < str.length; i += 2) {
+//     let count = Number(str[i + 1]);
+//     let temp = "";
+//     for (let j = 0; j < count; j++) {
+//       temp += str[i];
+//     }
+//     res += temp;
+//   }
+//   return res;
+// }
+// console.log(occChar("a2b3c4"));
+
+//! consider there is multiple number after each char
+
+function occChars(str) {
+  let result = "";
+  let currentChar = "";
+  let countStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (isNaN(char)) {
+      if (currentChar !== "" && countStr !== "") {
+        let count = parseInt(countStr);
+        for (let j = 0; j < count; j++) {
+          result += currentChar;
+        }
+      }
+      currentChar = char;
+      countStr = "";
+    } else {
+      countStr += char;
     }
-    res += temp;
   }
-  return res;
+
+  if (currentChar !== "" && countStr !== "") {
+    let count = parseInt(countStr);
+    for (let j = 0; j < count; j++) {
+      result += currentChar;
+    }
+  }
+
+  return result;
 }
-console.log(occChar("a2b3c4"));
+console.log(occChars("a20b3c4"));
