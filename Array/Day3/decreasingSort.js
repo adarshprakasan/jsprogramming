01deb257df2556e8dfa2e1e48bba5c03e379b9be
Adarsh <53871621+adarshprakasan@users.noreply.github.com>
2024-10-02 16:51:08 +0530

    array problems

--- Array/Day3/decreasingSort.js
+++ Array/Day3/decreasingSort.js
@@ -1,11 +1,10 @@
 //? Sort an array in decreasing order
 
 function decreaseSort(arr) {
-  let temp = arr[0];
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] < arr[j]) {
-        temp = arr[i];
+        let temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
       }
