90f2b582406925c68156d4dcd9962553c4183be1
Adarsh <53871621+adarshprakasan@users.noreply.github.com>
2024-10-05 16:54:30 +0530

    binarySearch program

--- Array/Day5/binarySearch.js
+++ Array/Day5/binarySearch.js
@@ -1,4 +1,18 @@
 //? Find the element is present inside array or not using binary search
 
-function binarySearch(arr, n) {}
-binarySearch([1, 2, 3, 4, 5], 2);
+function binarySearch(arr, n) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] === n) {
+      return true;
+    } else if (arr[mid] > n) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return false;
+}
+console.log(binarySearch([1, 2, 3, 4, 5], 2));
